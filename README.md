c6f93303e9f5c92d8e0359b6d3207b7bb64f4128
mharoot <michaelharootoonyan@yahoo.com>
2018-10-20 08:36:07 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# css
-Amazing Things You Can Do With Just CSS
+# SCSS 
+- A preprocessor which lets you use features that aren't a part of the wider CSS standard yet
+- Scss is an extension of the syntax of CSS supporting the full power of Sass.
+- [Check out the difference between css, Scss, and Sass.](https://stackoverflow.com/questions/46400443/what-is-the-difference-between-css-and-scss)
---
+++ christmas-lights/index.php
@@ -0,0 +1,64 @@
+<html lang="en" class=""><head>
+
+
+
+<script src="//static.codepen.io/assets/editor/live/console_runner-ce3034e6bde3912cc25f83cccb7caa2b0f976196f2f2d52303a462c826d54a73.js"></script>
+<script src="//static.codepen.io/assets/editor/live/css_reload-2a5c7ad0fe826f66e054c6020c99c1e1c63210256b6ba07eb41d7a4cb0d0adab.js"></script>
+<meta charset="UTF-8"><meta name="robots" content="noindex">
+<link rel="shortcut icon" type="image/x-icon" href="//static.codepen.io/assets/favicon/favicon-8ea04875e70c4b0bb41da869e81236e54394d63638a1ef12fa558a4a835f1164.ico"><link rel="mask-icon" type="" href="//static.codepen.io/assets/favicon/logo-pin-f2d2b6d2c61838f7e76325261b7195c27224080bc099486ddd6dccb469b8e8e6.svg" color="#111">
+<link rel="canonical" href="https://codepen.io/tobyj/pen/QjvEex">
+
+<link rel="stylesheet" href="style.scss">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+    <style class="cp-pen-styles">
+        
+    </style>
+</head>
+
+<body>
+<ul class="lightrope">
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+  <li></li>
+</ul>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+</body></html>
